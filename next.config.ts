d466897fd1dd461ec07b17d5391a5beac875fbd6
dev-dheeraj <dheerajnagar1998@gmail.com>
2025-04-01 13:49:35 +0530

    basic host fe setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "expense-host",
+        remotes: {
+          dashboard: "dashboard@http://localhost:3001/_next/static/chunks/remoteEntry.js",
+          expenses: "expenses@http://localhost:3002/_next/static/chunks/remoteEntry.js",
+        },
+        exposes: {},
+        shared: {},
+        extraOptions: {},
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
